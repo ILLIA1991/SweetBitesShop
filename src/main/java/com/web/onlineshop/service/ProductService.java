706e87070c1503f3e24a84a5a90e3208470cdb6a
Illia Belikau <ilasemcenkov1@gmail.com>
2023-11-25 16:34:26 -1100

    Created class ProductService

---
+++ src/main/java/com/web/onlineshop/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.web.onlineshop.service;
+
+import com.web.onlineshop.dto.ClientDTO;
+import com.web.onlineshop.dto.ProductDTO;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<ProductDTO> getAllProducts();
+
+    ProductDTO getById(Integer id);
+
+    Integer createProduct(ProductDTO productDTO);
+
+    void deleteById(Integer id);
+
+    ProductDTO updateProduct(Integer id, ProductDTO productsToUpdate);
+}
