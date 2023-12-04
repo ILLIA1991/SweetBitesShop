9a69d169edd8a0cd22fb81b2bb2b3d6f5b109731
Illia Belikau <ilasemcenkov1@gmail.com>
2023-12-03 16:24:47 -1100

    Create Enum NameFlavour

---
+++ src/main/java/com/web/onlineshop/repository/model/NameFlavours.java
@@ -0,0 +1,33 @@
+package com.web.onlineshop.repository.model;
+
+public enum NameFlavours {
+
+    BANANA("Banana"),
+    STRAWBERRY("Strawberry"),
+    CHERRY("Cherry"),
+    CHOCOLATE("Chocolate");
+
+    final String value;
+
+
+    public String getValue() {
+        return value;
+    }
+
+    NameFlavours(String value) {
+        this.value = value;
+    }
+
+    public static NameFlavours findByValue(String value) {
+        NameFlavours result = null;
+        for (NameFlavours flavours : values()) {
+            if(flavours.getValue().equalsIgnoreCase(value)) {
+                result = flavours;
+                break;
+            }
+
+        }
+        return result;
+    }
+
+}
