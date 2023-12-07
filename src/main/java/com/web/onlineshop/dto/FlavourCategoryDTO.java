e662a35ffbdeffb82985b67ae936171397ef3ba5
Illia Belikau <ilasemcenkov1@gmail.com>
2023-12-06 17:52:37 -1100

    Getters and Setters

--- src/main/java/com/web/onlineshop/dto/FlavourCategoryDTO.java
+++ src/main/java/com/web/onlineshop/dto/FlavourCategoryDTO.java
@@ -2,11 +2,69 @@ package com.web.onlineshop.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-@AllArgsConstructor
-@Data
+
+import java.util.Objects;
+
+
 public class FlavourCategoryDTO {
 
     private Integer id;
     private String name;
     private String description;
+
+    public FlavourCategoryDTO(Integer id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public FlavourCategoryDTO() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FlavourCategoryDTO that = (FlavourCategoryDTO) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description);
+    }
+
+    @Override
+    public String toString() {
+        return "FlavourCategoryDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/web/onlineshop/dto/ProductDTO.java
+++ src/main/java/com/web/onlineshop/dto/ProductDTO.java
@@ -4,12 +4,76 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.math.BigDecimal;
-@Data
-@AllArgsConstructor
+import java.util.Objects;
+
 public class ProductDTO {
     private Integer id;
     private String name;
     private FlavourCategoryDTO flavourCategoryDTO;
     private BigDecimal price;
 
+    public ProductDTO() {
+    }
+
+    public ProductDTO(Integer id, String name, FlavourCategoryDTO flavourCategoryDTO, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.flavourCategoryDTO = flavourCategoryDTO;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public FlavourCategoryDTO getFlavourCategoryDTO() {
+        return flavourCategoryDTO;
+    }
+
+    public void setFlavourCategoryDTO(FlavourCategoryDTO flavourCategoryDTO) {
+        this.flavourCategoryDTO = flavourCategoryDTO;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductDTO that = (ProductDTO) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(flavourCategoryDTO, that.flavourCategoryDTO) && Objects.equals(price, that.price);
+    }
+
+    @Override
+    public String toString() {
+        return "ProductDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", flavourCategoryDTO=" + flavourCategoryDTO +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, flavourCategoryDTO, price);
+    }
 }
