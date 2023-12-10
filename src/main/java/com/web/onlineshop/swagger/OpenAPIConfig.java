0f112556533778bdffe5936f33bb9f7c99b472ff
Maks <122127608+GromGT12@users.noreply.github.com>
2023-12-10 17:04:34 +0100

    Create Swagger

--- src/main/java/com/web/onlineshop/swagger/OpenAPIConfig.java
+++ src/main/java/com/web/onlineshop/swagger/OpenAPIConfig.java
@@ -1,8 +1,30 @@
 package com.web.onlineshop.swagger;
 
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenAPIConfig {
+    @Bean
+    public OpenAPI myOpenAPI() {
+        Contact contact = new Contact();
+        contact.setEmail("sweetorders@sweetbitesshop.com");
+        contact.setName("Masterpieces of Sweet Art: \"Creations for Your Taste\"");
+        contact.setUrl("https://github.com/ILLIA1991/SweetBitesShop");
 
+        License mitLicense = new License().name("Mit License").url("https://chonscalicense.com/Licenses/mit/;");
+
+        Info info = new Info()
+                .title("SweetBitesShop")
+                .version("1.0")
+                .contact(contact)
+                .description("API for SweetBitesShop System")
+                .license(mitLicense);
+        return new OpenAPI().info(info);
+    }
 }
