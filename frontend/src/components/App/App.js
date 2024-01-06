a9225e2a683a83a2de6dc92798bb4b606b9eeea5
Taras Senko <taras.senko.99@gmail.com>
2024-01-06 21:41:05 +0200

    add markup login page
    TODO: refractoring, add validation, send data to backend

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { Layout } from "../Layout/Layout";
 
 const Home = lazy(() => import("../../pages/Home/Home"));
+const Login = lazy(() => import("../../pages/Login/Login"));
 
 export const App = () => {
   return (
@@ -10,6 +11,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Route>
       </Routes>
